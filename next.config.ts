b56888377687cba50c349797020cbb16d65de402
Vitaliy <vitaliy60606@gmail.com>
2025-04-28 18:50:58 +0300

    настроен next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,14 @@ const nextConfig = {
         hostname: 'picsum.photos',
       },
     ],
+  },
+  webpack(config) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
