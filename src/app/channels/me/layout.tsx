33946d86bac3054bd5b7639621051d2e4256a001
Vitaliy <vitaliy60606@gmail.com>
2025-04-28 18:41:35 +0300

    все импорты вернул назад как было

--- src/app/channels/me/layout.tsx
+++ src/app/channels/me/layout.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { ReactNode, Suspense } from 'react';
-import Friends from '../../../components/sections/section.friends/section.friends';
-import AuthCheck from '../../../components/auth/authCheck';
-import Channels from '../../../components/sections/section.leftSideChannels/section.leftsideChannels';
-import Loading from '../../loading';
-import FetchUserInfo from '../../../components/auth/fetchUserInfo';
+import Friends from '@/components/sections/section.friends/section.friends';
+import AuthCheck from '@/components/auth/authCheck';
+import Channels from '@/components/sections/section.leftSideChannels/section.leftsideChannels';
+import Loading from '@/app/loading';
+import FetchUserInfo from '@/components/auth/fetchUserInfo';
 
 export default function ChannelsLayout({
   children,
