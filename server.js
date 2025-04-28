e710dbf60f1e9606c2222accdcc89501ac504438
Vitaliy <vitaliy60606@gmail.com>
2025-04-28 18:25:15 +0300

    tried to fix server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import { createServer } from 'node:http';
 import next from 'next';
 import { Server } from 'socket.io';
 
-const dev = process.env.NODE_ENV.toString() != 'production';
+const dev = process.env.NODE_ENV !== 'production';
 //const hostname = 'localhost';
-const hostname = dev ? 'localhost' : 'chat-web-app-sage.vercel.app';
+const hostname = dev ? 'localhost' : 'chat-web-app-awac.onrender.com';
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
